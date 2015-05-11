500ed2903f4d50eec059dd939aa5d2c900c0b0b3
coloredpanda <ok4luck@gmail.com>
2015-05-11 18:52:41 +0300

    Project built and ready to go.

--- Lobster.PageObjectModel/Dialogs/UpdatePasswordDialog.cs
+++ Lobster.PageObjectModel/Dialogs/UpdatePasswordDialog.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.SqlServer.Server;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
 
 namespace Lobster.PageObjectModel.Dialogs
